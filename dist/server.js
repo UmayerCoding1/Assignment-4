a2e4dabb9d1af53a7d3f37a10789774580e092e8
umayerCoding1 <umayerpro175@gmail.com>
2026-07-11 08:57:29 +0600

    feat: generate Prisma client artifacts and initialize Prisma service configuration

--- dist/server.js
+++ dist/server.js
@@ -5,34 +5,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const config_1 = __importDefault(require("./config"));
-let server;
-async function bootstrap() {
+const prisma_1 = require("./lib/prisma");
+const PORT = config_1.default.port;
+async function main() {
     try {
-        server = app_1.default.listen(config_1.default.port, () => {
-            console.log(`🚀 Server is running on port ${config_1.default.port}`);
-            console.log(`🔗 Health check available at http://localhost:${config_1.default.port}/health`);
+        await prisma_1.prisma.$connect();
+        console.log("Database connected");
+        app_1.default.listen(PORT, () => {
+            console.log(`Server running port:${PORT}`);
         });
     }
     catch (error) {
-        console.error('Failed to start server:', error);
+        console.error("Error starting the server:", error);
+        await prisma_1.prisma.$disconnect();
         process.exit(1);
     }
 }
-bootstrap();
-// Handle unhandled promise rejections
-process.on('unhandledRejection', (err) => {
-    console.log(`😈 Unhandled rejection detected, shutting down server...`, err);
-    if (server) {
-        server.close(() => {
-            process.exit(1);
-        });
-    }
-    else {
-        process.exit(1);
-    }
-});
-// Handle uncaught exceptions
-process.on('uncaughtException', (err) => {
-    console.log(`😈 Uncaught exception detected, shutting down...`, err);
-    process.exit(1);
-});
+main();
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,11 @@
-import { PrismaClient } from '../generated/prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
-import config from '../config';
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
 
-// Create pg connection pool
-const pool = new Pool({
-  connectionString: config.database_url,
-});
 
-// Create Prisma PostgreSQL adapter
-const adapter = new PrismaPg(pool);
+const connectionString = `${process.env.DATABASE_URL}`;
 
-// Instantiate and export Prisma Client
-export const prisma = new PrismaClient({
-  adapter,
-});
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
